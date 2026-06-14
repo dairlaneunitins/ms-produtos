9afe21677fd286edfca47408eb7ea740da9423fe
darleneunnitins <dairlanelina@unitins.br>
2026-06-13 23:35:44 -0300

    Atualizando projeto ms-produtos

---
+++ src/main/java/MsProdutosApplication.java
@@ -0,0 +1,12 @@
+package br.com.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProdutosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsProdutosApplication.class, args);
+    }
+}
